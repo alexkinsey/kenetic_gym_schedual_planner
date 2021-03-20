2babd87bc34e938074ddac98c111bafd7f1bf99a
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 19:21:23 +0000

    location repo CRUD functions created

--- repositories/location_repo.py
+++ repositories/location_repo.py
@@ -1,2 +1,31 @@
-from db.run_sql import sql
+from db.run_sql import run_sql
 from models.location import Location
+
+def save(location):
+    sql = "INSERT INTO locations (name) VALUES ( %s ) RETURNING id"
+    values = [location.name]
+    results = run_sql( sql, values )
+    location.id = results[0]['id']
+    return location
+
+def select_all():
+    locations = []
+
+    sql = "SELECT * FROM locations"
+    results = run_sql(sql)
+
+    for row in results:
+        location = Location(row['name'], row['id'])
+        locations.append(location)
+    return locations
+
+
+def select(id):
+    location = None
+    sql = "SELECT * FROM locations WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        location = Location(result['name'], result['category'], result['id'] )
+    return location
