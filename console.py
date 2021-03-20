585512f285a419280cb00fcd4026ba8bbce09313
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 20:27:26 +0000

    more data for db

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ import repositories.customer_repo as customer_repo
 
 location_repo.delete_all()
 trainer_repo.delete_all()
+customer_repo.delete_all()
 
 location1 = Location('Studio 1')
 location_repo.save(location1)
@@ -26,3 +27,11 @@ trainer_repo.save(trainer3)
 
 customer1 = Customer('Shaun', 'Hodge', 'standard', '2021/01/10', 'EH5 3RU', '07596685374', 'shaun.hodge@email.com')
 customer_repo.save(customer1)
+customer2 = Customer('Jade', 'Breslin', 'premium', '2020/08/13', 'EH12 1SU', '07759244212', 'jade.breslin@email.com')
+customer_repo.save(customer2)
+customer3 = Customer('Lucy', 'Atkinson', 'premium', '2019/10/25', 'EH2 6SR', '07321844372', 'lucy.atkinson@email.com')
+customer_repo.save(customer3)
+customer4 = Customer('James', 'Holden', 'standard', '2019/06/13', 'EH7 9DE', '07538472658', 'james.holden@email.com')
+customer_repo.save(customer4)
+customer5 = Customer('Gary', 'McDonald', 'standard', '2021/01/14', 'EH15 7ST', '07695844449', 'gary.mcdonals@email.com')
+customer_repo.save(customer5)
