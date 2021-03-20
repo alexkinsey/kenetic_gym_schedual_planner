4d821cf4dda4b3d999b2ad7aaacad098c7304d98
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 19:53:15 +0000

    customer class created

---
+++ models/customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    def __init__(self, first_name, last_name, membership, join_date, post_code, phone_number, email, id = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.membership = membership
+        self.join_date = join_date
+        self.post_code = post_code
+        self.phone_number = phone_number
+        self.email = email
+        self.id = id
+        
