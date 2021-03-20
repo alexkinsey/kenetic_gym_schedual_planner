a18bddfab35ac6ab190e8a34bf1e666134790f94
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 22:55:58 +0000

    attendance class created

---
+++ models/attendance.py
@@ -0,0 +1,6 @@
+class Attendance:
+    def __init__(self, customer_id, fitness_class_id, id):
+        self.customer_id = customer_id
+        self.fitness_class_id = fitness_class_id
+        self.id = id
+        
