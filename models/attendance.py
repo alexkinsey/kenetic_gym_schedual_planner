87107db9caeef349518c39abe65e231f329cfa80
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 23:00:09 +0000

    reordered attributes

--- models/attendance.py
+++ models/attendance.py
@@ -1,6 +1,5 @@
 class Attendance:
-    def __init__(self, customer_id, fitness_class_id, id):
-        self.customer_id = customer_id
+    def __init__(self, fitness_class_id, customer_id, id):
         self.fitness_class_id = fitness_class_id
+        self.customer_id = customer_id
         self.id = id
-        
