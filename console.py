5c333237ee926f7692a2d6b7ad3feb3138c9d30c
alexkinsey <alexanderkinsey@icloud.com>
2021-03-20 19:48:52 +0000

    test data created for db

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 # code to set up db
+from models.location import Location
+from models.location import Location
+from models.trainer import Trainer
+
+import repositories.location_repo as location_repo
+import repositories.trainer_repo as trainer_repo
+
+location_repo.delete_all()
+trainer_repo.delete_all()
+
+location1 = Location('Studio 1')
+location_repo.save(location1)
+location2 = Location('Studio 2')
+location_repo.save(location2)
+location3 = Location('Fitness Arena')
+location_repo.save(location3)
+
+trainer1 = Trainer('Zack', 'Black')
+trainer_repo.save(trainer1)
+trainer2 = Trainer('Kirsty', 'Whitehall')
+trainer_repo.save(trainer2)
+trainer3 = Trainer('Jamie', 'Gray')
+trainer_repo.save(trainer3)
--- repositories/trainer_repo.py
+++ repositories/trainer_repo.py
@@ -2,7 +2,7 @@ from db.run_sql import run_sql
 from models.trainer import Trainer
 
 def save(trainer):
-    sql = "INSERT INTO locations (first_name, last_name) VALUES ( %s, %s ) RETURNING id"
+    sql = "INSERT INTO trainers (first_name, last_name) VALUES ( %s, %s ) RETURNING id"
     values = [trainer.first_name, trainer.last_name]
     results = run_sql( sql, values )
     trainer.id = results[0]['id']
