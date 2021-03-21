82abcc6162cdb83fc9080d979fcc17e29b6b3955
alexkinsey <alexanderkinsey@icloud.com>
2021-03-21 13:24:51 +0000

    create CRUD action to get all classes for a member

--- repositories/member_repo.py
+++ repositories/member_repo.py
@@ -1,5 +1,9 @@
 from db.run_sql import run_sql
 from models.member import Member
+from models.fitness_class import FitnessClass
+
+import repositories.trainer_repo as trainer_repo
+import repositories.location_repo as location_repo
 
 def save(member):
     sql = """
@@ -57,3 +61,21 @@ def update(member):
     member.membership, member.join_date, 
     member.post_code, member.phone_number, member.email, member.id]
     run_sql(sql, values)
+
+def fitness_classes(member):
+    values = [member.id]
+    sql = """
+        SELECT fitness_classes.* FROM fitness_classes 
+        INNER JOIN attendance 
+        ON fitness_classes.id = attendance.fitness_class_id
+        WHERE member_id = %s
+    """
+    results = run_sql(sql, values)
+
+    fitness_classes = []
+    for row in results:
+        trainer = trainer_repo.select(row['trainer_id'])
+        location = location_repo.select(row['location_id'])
+        fitness_class = FitnessClass(row['title'], trainer, location, row['date'], row['time'], row['capacity'], row['id'])
+        fitness_classes.append(fitness_class)
+    return fitness_classes
