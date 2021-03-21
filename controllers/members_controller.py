6836959fa90376f1dc58bcdb7bba0410b55a3482
alexkinsey <alexanderkinsey@icloud.com>
2021-03-21 13:39:51 +0000

    added route to show member's info and classes

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -4,7 +4,15 @@ import repositories.member_repo as member_repo
 
 members_blueprint = Blueprint('members', __name__)
 
-@members_blueprint.route('/members', methods=["GET"])
+# show all members
+@members_blueprint.route('/members', methods=['GET'])
 def index():
     members = member_repo.select_all()
     return render_template('/members/index.html', all_members=members)
+
+# show selected member
+@members_blueprint.route('/members/<id>', methods=['GET'])
+def show(id):
+    found_member = member_repo.select(id)
+    found_fitness_classes = member_repo.fitness_classes(found_member)
+    return render_template('/members/show.html', member=found_member, fitness_classes=found_fitness_classes)
