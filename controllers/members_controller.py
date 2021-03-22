7cae4f2884fb286ef546edb41c5e52586335b534
alexkinsey <alexanderkinsey@icloud.com>
2021-03-22 11:05:24 +0000

    create form and route to make a new member

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -10,9 +10,14 @@ def index():
     members = member_repo.select_all()
     return render_template('/members/index.html', all_members=members)
 
-# show selected member
+# show selected member with all classes they are attending
 @members_blueprint.route('/members/<id>', methods=['GET'])
 def show(id):
     found_member = member_repo.select(id)
     found_fitness_classes = member_repo.fitness_classes(found_member)
     return render_template('/members/show.html', member=found_member, fitness_classes=found_fitness_classes)
+
+# create a new member form
+@members_blueprint.route('/members/new', methods=['GET'])
+def create_form():
+    return render_template('/members/new.html')
