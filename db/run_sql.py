f48056f743f7ac621ab3f0732ec86f976ccda3bd
alexkinsey <alexanderkinsey@icloud.com>
2021-06-17 15:20:16 +0100

    attempt to connect db

--- db/run_sql.py
+++ db/run_sql.py
@@ -6,6 +6,10 @@ def run_sql(sql, values = None):
     results = []
 
     try:
+        urlparse.uses_netloc.append("postgres") 
+        connection_params = urlparse.urlparse(os.environ["DATABASE_URL"])
+        db_connection = psycopg2.connect(database = connection_params.path[1:], user = connection_params.username, password = connection_params.password, host = connection_params.hostname, port = connection_params.port)
+
         conn=psycopg2.connect("dbname='gym_core'")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
