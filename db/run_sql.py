0e41a50edd1e4d9d8bf21b48c8de2595963d7bc0
alexkinsey <alexanderkinsey@icloud.com>
2021-06-17 15:46:59 +0100

    attempt to connect db

--- db/run_sql.py
+++ db/run_sql.py
@@ -6,7 +6,8 @@ def run_sql(sql, values = None):
     results = []
 
     try:
-        conn=psycopg2.connect("dbname='gym_core'")
+        # conn=psycopg2.connect("dbname='gym_core'")
+        conn=psycopg2.connect("DATABASE_URL")
         cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
